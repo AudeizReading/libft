9caeb7b221e11c417b90db0ba45b0b633950ea6e
AudeizReading <aaericia@hotmail.fr>
2021-03-22 22:05:33 +0100

    Updating libft.h

--- libft.h
+++ libft.h
@@ -13,20 +13,29 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 /*void	*ft_memset(void *b, int c, size_t len);
 void	bzero(void *s, size_t n);
 void	*memcpy(void *dst, const void *src, size_t n);
 void	*memccpy(void *dst, const void *src, int c, size_t n);
 void	*memmove(void *dst, const void *src, size_t len);
 void	*memchr(const void *s, int c, size_t n);
+int		memcmp(const void *s1, const void *s2, size_t n);
+size_t	strlen(const char *s);
+int		strncmp(const char *s1, const char *s2, size_t n);
+size_t	strlcpy(char *dst, const char *src, size_t size);
+size_t	strlcat(char *dst, const char *src, size_t size);
+char	*strnstr(const char *s1, const char *s2, size_t len);
 char	*ft_substr(char const *s, unsigned int start, size_t len);*/
+char	*strchr(const char *s, int c);
+char	*strrchr(const char *s, int c);
+int		atoi(const char *nptr);
 char	*ft_join(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
