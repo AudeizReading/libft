66be45e8c740d17a446b7c0b2998be6571aba833
AudeizReading <aaericia@hotmail.fr>
2021-03-22 19:37:30 +0100

    Correcting mistakes

--- libft.h
+++ libft.h
@@ -20,10 +20,10 @@ int	ft_isascii(int c);
 int	ft_isprint(int c);
 int	ft_toupper(int c);
 int	ft_tolower(int c);
-void	*ft_memset(void *b, int c, size_t len);
+/*void	*ft_memset(void *b, int c, size_t len);
 void	bzero(void *s, size_t n);
 void	*memcpy(void *dst, const void *src, size_t n);
 void	*memccpy(void *dst, const void *src, int c, size_t n);
 void	*memmove(void *dst, const void *src, size_t len);
-void	*memchr(const void *s, int c, size_t n);
+void	*memchr(const void *s, int c, size_t n);*/
 #endif
