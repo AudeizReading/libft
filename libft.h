6a6da28abb0a97fc53b76118a99654e8a27b4ccf
AudeizReading <aaericia@hotmail.fr>
2021-03-22 21:40:48 +0100

    Update libft.h

--- libft.h
+++ libft.h
@@ -25,5 +25,15 @@ void	bzero(void *s, size_t n);
 void	*memcpy(void *dst, const void *src, size_t n);
 void	*memccpy(void *dst, const void *src, int c, size_t n);
 void	*memmove(void *dst, const void *src, size_t len);
-void	*memchr(const void *s, int c, size_t n);*/
+void	*memchr(const void *s, int c, size_t n);
+char	*ft_substr(char const *s, unsigned int start, size_t len);*/
+char	*ft_join(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr(int n, int fd);
 #endif
