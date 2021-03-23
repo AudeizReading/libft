d1eef1e30d0348d48a242500f6cb94ad8bd06acf
alellouc <alellouc@student.42nice.fr>
2021-03-23 13:50:59 +0100

    Compliance to nirminette

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 11:33:15 by alellouc          #+#    #+#             */
-/*   Updated: 2021/03/23 11:55:11 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/03/23 13:49:12 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,16 @@ char	*ft_strdup(const char *s1)
 
 	len_s1 = ft_strlen(s1);
 	i = 0;
-	if ((dest = (char *)malloc(sizeof(char) * (len_s1 + 1))) == 0)
+	dest = (char *)malloc(sizeof(char) * (len_s1 + 1));
+	if (dest == 0)
 	{
 		errno = ENOMEM;
 		return ((char *)0);
 	}
-	while ((dest[i] = s1[i]) != '\0')
+	while (s1[i] != '\0')
+	{
+		dest[i] = s1[i];
 		i++;
+	}
 	return (dest);
 }
