e9f03e4ac103e97cd4588940e18e5b7705a45536
AudeizReading <aaericia@hotmail.fr>
2021-03-27 20:49:14 +0100

    updating ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 09:51:26 by alellouc          #+#    #+#             */
-/*   Updated: 2021/03/27 20:13:48 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/03/27 20:49:08 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,29 @@
 
 int	ft_atoi(const char *nptr)
 {
+	int			polarity;
 	int			sign;
-	int			minus;
 	long int	atoi;
 
-	sign = 1;
-	minus = 0;
+	polarity = 1;
+	sign = 0;
 	atoi = 0;
 	while (ft_isspace(*nptr))
 		nptr++;
-	while (*nptr == 45)
+	while (*nptr == 45 || *nptr == 43)
 	{
+		if (*nptr == 45)
+			polarity = -1;
+		sign++;
 		nptr++;
-		minus++;
-		sign = -1;
 	}
-	if (minus > 1)
+	if (sign > 1)
 		return (0);
 	while (ft_isdigit(*nptr))
 	{
 		atoi = atoi * 10 + *nptr - '0';
 		nptr++;
 	}
-	atoi *= sign;
+	atoi *= polarity;
 	return ((int)atoi);
 }
