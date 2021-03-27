af945e56ebe2d9db84d10401c2528707f1314910
AudeizReading <aaericia@hotmail.fr>
2021-03-27 17:39:50 +0100

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 15:52:25 by alellouc          #+#    #+#             */
-/*   Updated: 2021/03/27 17:13:04 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/03/27 17:38:30 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,9 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	
 	p_dst = (unsigned char *)dst;
 	p_src = (const unsigned char *)src;
-	while (n)
-	{
+	if (!dst)
+		return (NULL);
+	while (n--)
 		*p_dst++ = *p_src++;
-		n--;
-	}
 	return (dst);
 }
