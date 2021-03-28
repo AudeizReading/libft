4d9440524ed270007b4741d9e313ddd3dbb6b65e
AudeizReading <aaericia@hotmail.fr>
2021-03-28 20:53:35 +0200

    ft_calloc:update the function

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 13:52:23 by alellouc          #+#    #+#             */
-/*   Updated: 2021/03/28 20:25:35 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/03/28 20:39:03 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	unsigned char	*buffer;
 
+	if (!count || !size)
+	{
+		count = 1;
+		size = 1;
+	}
 	buffer = malloc(size * count);
 	if (buffer == NULL)
 		return (NULL);
