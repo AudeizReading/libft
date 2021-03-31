8ab5f37e71f2109d336adea1a02c295f44b10c66
alellouc <alellouc@student.42nice.fr>
2021-03-31 10:16:54 +0200

    ft_memcpy: updating fn

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/28 16:17:17 by alellouc          #+#    #+#             */
-/*   Updated: 2021/03/28 16:31:58 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/03/31 10:15:14 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 
 	p_dst = dst;
 	p_src = src;
+	if (!p_dst && !p_src)
+		return ((void *)0);
 	if (p_dst < p_src)
 		return (ft_memcpy(p_dst, p_src, len));
 	while (len--)
