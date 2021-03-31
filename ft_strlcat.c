2a44400c443d77d14549e7ecd8f6ecb5d32782aa
alellouc <alellouc@student.42nice.fr>
2021-03-31 17:50:46 +0200

    Working on ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 10:45:05 by alellouc          #+#    #+#             */
-/*   Updated: 2021/03/31 16:21:29 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/03/31 17:49:58 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,20 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t len_dst;
 	size_t len_src;
-	size_t i;
-	size_t ret;
+/*	size_t i;
+	size_t ret;*/
 
 	len_dst = ft_strlen(dst);
-	len_src = ft_strlen(src);
+	len_src = ft_strlcpy((dst + len_dst), src, size);
+/*	len_src = ft_strlen(src);
 	i = 0;
 	if (!dst)
 		len_dst = size;
 	ret = len_dst + len_src;
 	while (*src && size--)
-		dst[len_dst++] = *src++;
+		dst[len_dst++] = *src++;*/
 		/*dst[len_dst + i++] = *src++;*/
-	dst[len_dst] = '\0';
+/*	dst[len_dst] = '\0';*/
 /*	return (ret);*/
-	return (len_dst);
+	return (len_dst + len_src);
 }
