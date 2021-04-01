bd690ee2ff25397340a12bc8853e95e18543c2ad
alellouc <alellouc@student.42nice.fr>
2021-04-01 08:36:01 +0200

    ft_strlcat: append at the end of dst the src for size chars only. Return src_len + dst_len, or size + dst_len if size < len_dst

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 10:45:05 by alellouc          #+#    #+#             */
-/*   Updated: 2021/03/31 17:49:58 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/04/01 08:31:20 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,18 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t len_dst;
 	size_t len_src;
-/*	size_t i;
-	size_t ret;*/
+	size_t result;
 
 	len_dst = ft_strlen(dst);
-	len_src = ft_strlcpy((dst + len_dst), src, size);
-/*	len_src = ft_strlen(src);
-	i = 0;
-	if (!dst)
-		len_dst = size;
-	ret = len_dst + len_src;
-	while (*src && size--)
-		dst[len_dst++] = *src++;*/
-		/*dst[len_dst + i++] = *src++;*/
-/*	dst[len_dst] = '\0';*/
-/*	return (ret);*/
-	return (len_dst + len_src);
+	len_src = ft_strlen(src);
+	result = len_dst + len_src;
+	if (len_dst >= size)
+		return (size + len_src);
+	dst += len_dst;
+	size -= len_dst;
+	if (len_src >= size)
+		len_src = size - 1;
+	ft_memcpy(dst, src, len_src);
+	dst[len_src] = '\0';
+	return (result);
 }
