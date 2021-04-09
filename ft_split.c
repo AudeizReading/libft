7221b29a837b4dd0838438d48959168d98d5c442
alellouc <alellouc@student.42nice.fr>
2021-04-09 22:37:52 +0200

    update ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 16:27:54 by alellouc          #+#    #+#             */
-/*   Updated: 2021/04/09 16:32:42 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/04/09 22:35:21 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,35 +18,19 @@ char	**ft_split(char const *s, char c)
 	int		nb_wds;
 	int		i;
 
-	ft_putendl_fd("\n=======================================", 1);
-	/*ft_putstr_fd("Chaine a split: ", 1);
-	ft_putendl_fd((char *)s, 1);*/
 	i = 0;
 	if (!c)
-	{
-		ft_putendl_fd("Pas de separateur", 1);
 		nb_wds = 1;
-	}
 	if (!s)
-	{
-		ft_putendl_fd("Pas de chaine", 1);
 		nb_wds = 0;
-	}
 	else
 		nb_wds = ft_cntwds((char *)s, (char)c);
-/*	ft_putendl_fd("Nb de mots trouves:", 1);
-	ft_putnbr_fd(nb_wds, 1);
-	ft_putchar_fd('\n', 1);*/
 	tab = (char **)ft_calloc(nb_wds + 1, sizeof(*tab));
 	if (!tab)
 		return (NULL);
 	if (nb_wds > 1)
- 	{
-	while (ft_ischarset(*s, (char const *)&c))
-		s++;
-	}
-	/*ft_putendl_fd("Check valeur de s, avant assignation de tab:", 1);
-	ft_putendl_fd((char *)s, 1);*/
+		while (ft_ischarset(*s, (char const *)&c))
+			s++;
 	while (i < nb_wds)
 	{
 		tab[i] = ft_strshift(s, (char const *)&c);
@@ -57,20 +41,12 @@ char	**ft_split(char const *s, char c)
 			free(tab);
 			return (NULL);
 		}
-	/*	ft_putnbr_fd(i, 1);
-		ft_putstr_fd(" : ", 1);
-		ft_putendl_fd(tab[i], 1);
-		ft_putchar_fd('\n', 1);*/
 		if (nb_wds > 1)
 		{
-		while (!ft_ischarset(*s, (char const *)&c))
-			s++;
-	/*	ft_putendl_fd("Check valeur de s, apres assignation:", 1);
-		ft_putendl_fd((char *)s, 1);*/
-		while (ft_ischarset(*s, (char const *)&c))
-			s++;
-	/*	ft_putendl_fd("Check valeur de s, apres assignation:", 1);
-		ft_putendl_fd((char *)s, 1);*/
+			while (!ft_ischarset(*s, (char const *)&c))
+				s++;
+			while (ft_ischarset(*s, (char const *)&c))
+				s++;
 		}
 		i++;
 	}
