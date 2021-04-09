4f11da4e73144e1abe2f762e8cd849a8a4e2fb85
alellouc <alellouc@student.42nice.fr>
2021-04-09 22:41:40 +0200

    compliance to norminette

--- ft_cntwds.c
+++ ft_cntwds.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 17:18:21 by alellouc          #+#    #+#             */
-/*   Updated: 2021/04/09 12:37:41 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/04/09 22:39:51 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 int	ft_cntwds(char *str, int sep)
 {
-	enum	e_state
-	{
-		OUTSIDE_WD,
-		INSIDE_WD
-	};
 	int		nb;
 	int		state;
 	char	c_sep;
 
+	enum e_state
+	{
+		OUTSIDE_WD,
+		INSIDE_WD
+	};
 	nb = 0;
 	c_sep = (char)sep;
 	state = OUTSIDE_WD;
