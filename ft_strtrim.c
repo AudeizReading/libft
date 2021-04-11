6f5c483b613b865c9d751b43b8a469b233c93bd0
alellouc <alellouc@student.42nice.fr>
2021-04-11 16:41:43 +0200

    secure ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/04 17:21:01 by alellouc          #+#    #+#             */
-/*   Updated: 2021/04/08 12:08:49 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/04/11 16:39:59 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	start;
 	size_t	len_dst;
 
+	if (!s1)
+		return (NULL);
 	start = 0;
 	len_dst = ft_strlen(s1);
 	while (ft_ischarset(s1[start], set))
