163e3d3a8ef0b322ac2da27649e20635fd404896
AudeizReading <aaericia@hotmail.fr>
2021-04-12 12:34:56 +0200

    make some tminiks for pushing so adding shit to my code for pushing

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/04 17:21:01 by alellouc          #+#    #+#             */
-/*   Updated: 2021/04/12 12:33:51 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/04/12 12:34:49 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (NULL);
 	if (!set)
 		return ((char *)s1);
-	/*blabla to push*/
 	start = 0;
 	len_dst = ft_strlen(s1);
 	while (ft_ischarset(s1[start], set))
