cf47cbfc5b1af2dac11a6c08d1732b96b0eb9ee7
alellouc <alellouc@student.42nice.fr>
2021-04-15 12:48:34 +0200

    untry things on memcpy; don't pass alelievr

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 15:52:25 by alellouc          #+#    #+#             */
-/*   Updated: 2021/04/15 12:12:48 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/04/15 12:47:20 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 
 	p_dst = dst;
 	p_src = src;
-	/*if (!p_dst && !p_src)*/
-	if (!p_src || !p_dst)
+	if (!p_dst && !p_src)
 		return ((void *)0);
 	while (n--)
 		*p_dst++ = *p_src++;
