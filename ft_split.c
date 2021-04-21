08d886759a265655fd2a46dcfc0a2bb3cd60f865
AudeizReading <aaericia@hotmail.fr>
2021-04-21 12:10:55 +0200

    ft_split 2e version, brut de decoffrage, non refact, juste pour voir si elle passe les tests sur macOS

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 16:27:54 by alellouc          #+#    #+#             */
-/*   Updated: 2021/04/19 10:19:32 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/04/21 12:03:32 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_strtotab(char **tab, int i, char const *s, char c)
+/*static char	*ft_strtotab(char **tab, int i, char const *s, char c)
 {
 	tab[i] = ft_strshift(s, (char const *)&c);
 	if (!tab[i])
@@ -23,11 +23,11 @@ static char	*ft_strtotab(char **tab, int i, char const *s, char c)
 		return (NULL);
 	}
 	return (*(tab + i));
-}
+}*/
 
 char	**ft_split(char const *s, char c)
 {
-	char	**tab;
+	/*char	**tab;
 	int		nb_wds;
 	int		i;
 
@@ -49,5 +49,62 @@ char	**ft_split(char const *s, char c)
 		i++;
 	}
 	tab[i] = NULL;
-	return (tab);
+	return (tab);*/
+
+	/* ---------------------------------------------------------------------- */
+	char	**dst;
+	int		h;
+	int		i;
+	int		j;
+	int		k;
+	int		m;
+	int		nb;
+
+	h = 0;
+	i = 0;
+	j = 0;
+	k = 0;
+	m = 0;
+	nb = 0;
+	if (!s)
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == 0))
+			nb++;
+		i++;
+	}
+	i = 0;
+	dst = (char**)malloc((nb + 1) * sizeof(*dst));
+	if (!dst)
+		return (NULL);
+	while (i < nb)
+	{
+		while (s[j] == c)
+			j++;
+		while (s[j] && s[j] != c)
+		{
+			if (s[j] != c && (s[j - 1] == c || j - 1 < 0))
+				k = j;
+			if (s[j] != c && (s[j + 1] == c || s[j + 1] == 0))
+				m = j;
+			j++;
+		}
+		dst[i] = (char *)malloc(sizeof(**dst) * (m - k + 2));
+		if (!dst[i])
+		{
+			while (i >= 0)
+				free(dst[i--]);
+			free(dst);
+			return (NULL);
+		}
+		h = 0;
+		while (k <= m)
+		{
+			dst[i][h++] = s[k++];
+		}
+		dst[i++][h] = 0;
+	}
+	dst[i] = NULL;
+	return (dst);
 }
