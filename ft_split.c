632652b2dcfa15a001763c9c3a0dd39228bf6207
AudeizReading <aaericia@hotmail.fr>
2021-04-22 12:59:30 +0200

    update split retry1

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,61 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 16:27:54 by alellouc          #+#    #+#             */
-/*   Updated: 2021/04/21 12:21:24 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/04/22 12:57:36 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	ft_wdscnt(char const *s, char c)
+{
+	int		i;
+	int		nb;
+
+	i = 0;
+	nb = 0;
+	while (s[i])
+	{
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == 0))
+			nb++;
+		i++;
+	}
+	return (nb);
+}
+
+static int	ft_wdlen(int *offset_i, int *offset_s, char const *s, char c)
+{
+	static int	i = 0;
+
+	if ((size_t)i > (ft_strlen(s) - 2))
+		i = 0;
+	while (s[i] == c)
+		i++;
+	while (s[i] && s[i] != c)
+	{
+		if (s[i] != c && (s[i - 1] == c || i - 1 < 0))
+			*offset_i = i;
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == 0))
+			*offset_s = i;
+		i++;
+	}
+	while (s[i] == c)
+		i++;
+	return (*offset_s - *offset_i + 1);
+}
+
+static int	ft_isfree(char **tab, int index)
+{
+	if (!tab[index])
+	{
+		while (index >= 0)
+			free(tab[index--]);
+		free(tab);
+		return (1);
+	}
+	return (0);
+}
+
 
 /*static char	*ft_strtotab(char **tab, int i, char const *s, char c)
 {
@@ -34,7 +84,7 @@ char	**ft_split(char const *s, char c)
 	i = 0;
 	if (!s)
 		return (NULL);
-	nb_wds = ft_cntwds((char *)s, (char)c);
+	nb_wds = ft_wdscnt((char *)s, (char)c);
 	tab = (char **)ft_calloc(nb_wds + 1, sizeof(*tab));
 	if (!tab)
 		return (NULL);
@@ -53,57 +103,25 @@ char	**ft_split(char const *s, char c)
 
 	/* ------------ 2nd retry ---------------------------------------------------------- */
 	char	**dst;
-	int		h;
 	int		i;
-	int		j;
-	int		k;
-	int		m;
+	int		offset_i;
+	int		offset_s;
 	int		nb;
 
-	h = 0;
-	i = 0;
-	j = 0;
-	k = 0;
-	m = 0;
-	nb = 0;
 	if (!s)
 		return (NULL);
-	while (s[i])
-	{
-		if (s[i] != c && (s[i + 1] == c || s[i + 1] == 0))
-			nb++;
-		i++;
-	}
 	i = 0;
+	offset_i = 0;
+	offset_s = 0;
+	nb = ft_wdscnt(s, c);
 	dst = (char**)malloc((nb + 1) * sizeof(*dst));
 	if (!dst)
 		return (NULL);
 	while (i < nb)
 	{
-		while (s[j] == c)
-			j++;
-		while (s[j] && s[j] != c)
-		{
-			if (s[j] != c && (s[j - 1] == c || j - 1 < 0))
-				k = j;
-			if (s[j] != c && (s[j + 1] == c || s[j + 1] == 0))
-				m = j;
-			j++;
-		}
-		dst[i] = (char *)malloc(sizeof(**dst) * (m - k + 2));
-		if (!dst[i])
-		{
-			while (i >= 0)
-				free(dst[i--]);
-			free(dst);
+		dst[i] = ft_substr(s, offset_i, ft_wdlen(&offset_i, &offset_s, s, c));
+		if (ft_isfree(dst, i++))
 			return (NULL);
-		}
-		h = 0;
-		while (k <= m)
-		{
-			dst[i][h++] = s[k++];
-		}
-		dst[i++][h] = 0;
 	}
 	dst[i] = NULL;
 	return (dst);
